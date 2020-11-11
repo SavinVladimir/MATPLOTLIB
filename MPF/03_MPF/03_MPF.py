cddba58023203060f08e389a7debd3441c62790a
SavinVladimir <61633933+SavinVladimir@users.noreply.github.com>
2020-11-11 19:26:39 +0300

    Add files via upload

---
+++ MPF/03_MPF/03_MPF.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import mplfinance as mpf
+
+data = pd.read_csv('../AAPL.csv')
+
+
+def Date():
+    global data
+    data.Date = pd.to_datetime(data.Date)
+    data = data.set_index('Date')
+
+
+Date()
+
+mpf.plot(data['2020-09':'2020-10'], type='candle', volume=True)
---
+++ MPF/05_MPF/05_MPF.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import mplfinance as mpf
+
+data = pd.read_csv('../AAPL.csv')
+
+
+def Date():
+    global data
+    data.Date = pd.to_datetime(data.Date)
+    data = data.set_index('Date')
+
+
+Date()
+
+mpf.plot(data['2020-09':'2020-10'], figratio=(22, 12), type='candle',
+         mav=2, volume=True,title='AAPL', style='starsandstripes')
